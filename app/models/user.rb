3afecdd78bd8340b09348e4aa2ddaeb2ab2e675a
Shaun Carland <shaundreamsincode@gmail.com>
2023-07-01 02:19:41 -0600

    fix?

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   validates :password, presence: true, on: :create
 
   def generate_jwt
-    JWT.encode({ id: id, email: email }, Rails.application.secrets.secret_key_base, 'HS256')
+    JWT.encode({ id: id, email: email }, ENV['SECRET_KEY_BASE'], 'HS256')
   end
 
   def self.from_token(token)
-    decoded_token = JWT.decode(token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')
+    decoded_token = JWT.decode(token, ENV['SECRET_KEY_BASE'], true, algorithm: 'HS256')
     find(decoded_token.first['id'])
   rescue JWT::DecodeError
     nil
