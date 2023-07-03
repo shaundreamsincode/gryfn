3c0adca01f347d15bb94da5e4483e06a4b382633
Shaun Carland <shaundreamsincode@gmail.com>
2023-07-02 21:05:18 -0600

    get card structure working

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Routes from "../routes";
+import { Card, CardContent } from "@material-ui/core";
 
 export default props => <>
-    {Routes}
-    <br/> <br/>
-    DISCLAIMER GOES HERE!
+    <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+            {Routes}
+            <br/> <br/>
+            DISCLAIMER GOES HERE!
+        </CardContent>
+    </Card>
 </>;
--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,23 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-import { Button } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
 
-export default () => (
-    <div>
-        <h1 className="display-4">Docbot :D</h1>
-        <p className="lead">
-            Welcome!
-        </p>
-        <hr className="my-4" />
-        <Link
-            to="/chats/new"
-            className="btn btn-lg custom-button"
-            role="button"
-        >
-            Create a Nessw Chat
-        </Link>
 
-        <Button color="primary">Hello World!</Button>
-    </div>
-);
+const Home = () => {
+    const navigate = useNavigate()
+
+    const onNewChatClick = () => {
+        navigate('/chats/new')
+    }
+
+    return(
+        <>
+            <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                Welcome!
+            </Typography>
+
+            <Button onClick={onNewChatClick} color="primary">Create a New Chat</Button>
+        </>
+    )
+};
+
+export default Home
