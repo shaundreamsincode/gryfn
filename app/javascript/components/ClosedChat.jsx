41b535192b074f1e0b31eb83007418a9ded12846
Shaun Carland <shaundreamsincode@gmail.com>
2023-07-02 23:02:58 -0600

    add copy summary to clipboard functionality

--- app/javascript/components/ClosedChat.jsx
+++ app/javascript/components/ClosedChat.jsx
@@ -1,58 +1,90 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import ApiService from "../services/ApiService";
-import {Button, Typography} from "@material-ui/core";
+import { Button, Typography, Snackbar } from "@material-ui/core";
 
 const ClosedChat = (props) => {
     const { chat } = props;
-    const [summary, setSummary] = useState(null)
-    const [summaryLoading, setSummaryLoading] = useState(false)
+    const [summary, setSummary] = useState(null);
+    const [summaryLoading, setSummaryLoading] = useState(false);
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
+
+    const summaryContentRef = useRef(null);
 
     const onGenerateSummary = () => {
-        setSummaryLoading(true)
+        setSummaryLoading(true);
 
-        ApiService.post(`/api/v1/chats/${chat.token}/summaries`).then((response) => {
-            setSummary(response.data)
-            setSummaryLoading(false)
-        })
-    }
+        ApiService.post(`/api/v1/chats/${chat.token}/summaries`).then(
+            (response) => {
+                setSummary(response.data);
+                setSummaryLoading(false);
+            }
+        );
+    };
 
     const getGenerateSummaryButtonText = () => {
         if (summaryLoading) {
-            return 'Loading'
+            return "Loading";
         } else if (summary) {
-            return 'Summary Generated'
+            return "Summary Generated";
         } else {
-            return 'Generate Summary'
+            return "Generate Summary";
         }
-    }
+    };
 
-    return(
+    const onCopySummary = () => {
+        const summaryContent = summaryContentRef.current.innerText;
+        navigator.clipboard.writeText(summaryContent).then(() => {
+            console.log("Summary content copied to clipboard!");
+        });
+
+        setSnackbarOpen(true)
+    };
+
+    return (
         <>
-            <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                 Chat has been closed by the user.
             </Typography>
 
-            <Typography color="text.secondary" align="center" component="h1" variant="h4" gutterBottom>
-                <Button onClick={onGenerateSummary} disabled={summaryLoading || summary} color="primary">
-                    { getGenerateSummaryButtonText() }
+            <Typography
+                color="text.secondary"
+                align="center"
+                component="h1"
+                variant="h4"
+                gutterBottom
+            >
+                <Button
+                    onClick={onGenerateSummary}
+                    disabled={summaryLoading || summary}
+                    color="primary"
+                >
+                    {getGenerateSummaryButtonText()}
                 </Button>
             </Typography>
 
-            {
-                summary && <>
+            {summary && (
+                <>
                     <Typography variant="body2" color="text.secondary" gutterBottom>
-                        <div id="summaryContent">
-                            {summary.split('\n').map((item, index) => (
-                                <div key={index}>
-                                    {item}
-                                </div>
+                        <div id="summaryContent" ref={summaryContentRef}>
+                            {summary.split("\n").map((item, index) => (
+                                <div key={index}>{item}</div>
                             ))}
                         </div>
                     </Typography>
-                </>
-            }
-        </>
-    )
-}
 
-export default ClosedChat
+                    <Button onClick={onCopySummary} style={{ marginTop: '10px' }} align="right" color="primary">
+                        Copy Summary
+                    </Button>
+                    <Snackbar
+                        open={snackbarOpen}
+                        autoHideDuration={6000}
+                        onClose={() => setSnackbarOpen(false)}
+                        message="Text Copied to Clipboard!"
+                    />
+                </>
+            )}
+        </>
+    );
+};
+
+export default ClosedChat;
