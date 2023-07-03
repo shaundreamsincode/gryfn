fb0958e060fd1639c7cedfef66cf124d0df87013
Shaun Carland <shaundreamsincode@gmail.com>
2023-07-02 20:51:33 -0600

    get hello world bttn showing

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { Button } from '@material-ui/core';
+
 export default () => (
-    <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-        <div className="jumbotron jumbotron-fluid bg-transparent">
-            <div className="container secondary-color">
-                <h1 className="display-4">Docbot</h1>
-                <p className="lead">
-                    Welcome!
-                </p>
-                <hr className="my-4" />
-                <Link
-                    to="/chats/new"
-                    className="btn btn-lg custom-button"
-                    role="button"
-                >
-                    Create a New Chat
-                </Link>
-            </div>
-        </div>
+    <div>
+        <h1 className="display-4">Docbot :D</h1>
+        <p className="lead">
+            Welcome!
+        </p>
+        <hr className="my-4" />
+        <Link
+            to="/chats/new"
+            className="btn btn-lg custom-button"
+            role="button"
+        >
+            Create a Nessw Chat
+        </Link>
+
+        <Button color="primary">Hello World!</Button>
     </div>
 );
