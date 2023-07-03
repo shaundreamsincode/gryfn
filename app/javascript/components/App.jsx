c86304b40e0256d569ce65b8fe7ab110db2f2e77
Shaun Carland <shaundreamsincode@gmail.com>
2023-07-02 21:25:27 -0600

    get home page styling done

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Routes from "../routes";
-import { Card, CardContent } from "@material-ui/core";
+import { Container, Card, CardContent, Typography } from "@material-ui/core";
 
 export default props => <>
-    <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-            {Routes}
-            <br/> <br/>
-            DISCLAIMER GOES HERE!
-        </CardContent>
-    </Card>
+    <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+        <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+                <Typography component="h1" variant="h4" align="center">
+                    DocBot
+                </Typography>
+                {Routes}
+            </CardContent>
+            <CardContent>
+                <Typography variant="body2" color="text.secondary" align="center">
+                    DocBot is NOT a substitute for professional medical advice, diagnosis or treatment.
+                    Always seek the advice of your physician or other qualified health provider with any
+                    questions you may have regarding a medical condition. If you are having a
+                    medical emergency, call 911 or your local emergency services number.
+                </Typography>
+            </CardContent>
+        </Card>
+    </Container>
 </>;
--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -14,10 +14,17 @@ const Home = () => {
     return(
         <>
             <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
-                Welcome!
+                Welcome! DocBot is an AI powered chatbot designed to help your doctor better care for you.
+                DocBot will ask you a series of questions about the symptoms you are experiencing.
+                Please answer with as much detail as you are able to. At the end of the chat,
+                DocBot will generate a summary that you can provide to your doctor. Your doctor will
+                be able to review this summary and they may ask further clarifying questions.
+                Then, your doctor will talk to you about any possible recommended tests and make a treatment plan.
             </Typography>
 
-            <Button onClick={onNewChatClick} color="primary">Create a New Chat</Button>
+            <Typography color="text.secondary" align="center" component="h1" variant="h4" gutterBottom>
+                <Button onClick={onNewChatClick} color="primary">Create a New Chat</Button>
+            </Typography>
         </>
     )
 };
