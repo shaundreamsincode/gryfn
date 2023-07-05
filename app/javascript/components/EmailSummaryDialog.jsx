56c4dbe10988c32dba96a36585b15e784f6d9ba7
Shaun Carland <shaundreamsincode@gmail.com>
2023-07-05 17:48:11 -0600

    get EmailSummaryDialog working

--- app/javascript/components/EmailSummaryDialog.jsx
+++ app/javascript/components/EmailSummaryDialog.jsx
@@ -1,10 +1,44 @@
 import React, { useState } from 'react';
 
-import { Button, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@material-ui/core";
+import {
+    Button,
+    TextField,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Typography
+} from "@material-ui/core";
 
 function EmailSummaryDialog(props) {
     const [email, setEmail] = useState('')
+    const [isInvalid, setIsInvalid] = useState(false)
+
     const { open, closeDialog, dialogConfirmation } = props
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    const onEmailChange = (newEmail) => {
+        setEmail(newEmail)
+        setIsInvalid(false)
+    }
+
+    const onDialogClose = () => {
+        setIsInvalid(false)
+        closeDialog()
+    }
+
+    const onSubmit = () => {
+        setIsInvalid(false)
+
+        const emailIsValid = emailRegex.test(email)
+
+        if (emailIsValid) {
+            dialogConfirmation(email)
+        } else {
+            setIsInvalid(true)
+        }
+    }
 
     return (
         <>
@@ -13,10 +47,18 @@ function EmailSummaryDialog(props) {
                 <DialogContent>
                     <DialogContentText>Please enter the email you want to send the summary to.</DialogContentText>
                 </DialogContent>
-                <TextField onChange={(event) => setEmail(event.target.value)}/>
+
+                {
+                    isInvalid &&
+                        <DialogContentText variant="caption" align="center" style={{ color: 'red' }}>Email is invalid</DialogContentText>
+                }
+
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <TextField style={{ width: '75%' }} onChange={(event) => onEmailChange(event.target.value)} />
+                </div>
                 <DialogActions>
-                    <Button onClick={closeDialog} disabled={email.length > 0}>Cancel</Button>
-                    <Button onClick={() => dialogConfirmation(email)} autoFocus>
+                    <Button onClick={onDialogClose} color="primary">Cancel</Button>
+                    <Button onClick={onSubmit} autoFocus color="primary" disabled={email.length === 0}>
                         Confirm
                     </Button>
                 </DialogActions>
