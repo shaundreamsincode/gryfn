5d2b482f076aee0064dc93d3bf5f046f9755a4d5
Shaun Carland <shaundreamsincode@gmail.com>
2023-08-07 14:28:47 -0600

    fix pages ctrl

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,5 @@
 class PagesController < ApplicationController
   def home
-    respond_to do |format|
-      Event.create(name: 'home_page_viewed')
-
-      format.html # Render the default HTML template (if required)
-      format.css  # Render the CSS template for the "text/css" format
-    end
   end
 
   def login
