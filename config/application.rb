6359f59df9a36409fbaac735946ecf72a58e8db5
Shaun Carland <shaundreamsincode@gmail.com>
2023-08-06 22:16:53 -0600

    fix

--- config/application.rb
+++ config/application.rb
@@ -10,21 +10,5 @@ module Docbot
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    config.action_mailer.delivery_method = :postmark
-
-    config.action_mailer.delivery_method = :postmark
-
-    config.action_mailer.postmark_settings = {
-      api_token: ENV['POSTMARK_API_TOKEN']
-    }
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
