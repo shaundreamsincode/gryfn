bd957d60c78aa8d3484f13e387dd517b3eaa598f
Shaun Carland <shaundreamsincode@gmail.com>
2023-08-07 15:02:19 -0600

    clean out FE

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,40 +1,10 @@
 import React, { useState } from "react";
-import Disclaimer from "./domains/footer/Disclaimer";
 import { useNavigate } from "react-router-dom";
-import LanguageService from "../services/LanguageService";
-import {Button, CardContent, Typography} from '@material-ui/core';
-import TranslateService from "../services/LanguageService";
-import Footer from "./domains/footer/Footer";
 
 const Home = () => {
-    const initialLanguage = localStorage.getItem('lang') || 'en'
-    const [language, setLanguage] = useState(initialLanguage)
-    const navigate = useNavigate()
-
-    const onLanguageChange = () => {
-        const newLanguage = language === 'en' ? 'es' : 'en'
-        localStorage.setItem('lang', newLanguage)
-        setLanguage(newLanguage)
-    }
-
-    const languageChangeButtonText = language === 'en' ? 'Español' : 'English'
 
     return(
         <>
-            <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
-                {
-                    LanguageService.translate('about')
-                }
-            </Typography>
-
-            <Typography color="text.secondary" align="center" component="h1" variant="h4" gutterBottom>
-                <Button onClick={() => navigate('/chats/new')} color="primary">
-                    { LanguageService.translate('createChatButton', language) }
-                </Button>
-            </Typography>
-
-            <Button onClick={onLanguageChange} color="primary">{ languageChangeButtonText }</Button>
-            <Footer lang={language}/>
         </>
     )
 };
--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -2,46 +2,13 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "../components/Home";
-import Chat from "../components/domains/chat/Chat";
-import ContactUs from "../components/ContactUs";
-import Analytics from "../components/domains/analytics/Analytics";
-import PrivacyPolicy from "../components/domains/privacy_policy/PrivacyPolicy";
 
 export default (
     <Router>
         <Fragment>
             <Routes>
                 <Route exact path='/' element={<Home/>}/>
-                <Route exact path='/contact' element={<ContactUs/>}/>
-                <Route exact path='/analytics' element={<Analytics/>}/>
-                <Route path='/chats/:token' element={<Chat/>}/>
-                <Route path='/chats' element={<Chat/>}/>
-
-                <Route path="privacy_policy" element={<PrivacyPolicy/>}/>
-
-                {/*<Route exact path='/' element={<PrivateRoute/>}>*/}
-                {/*    <Route exact path='/' element={<Home/>}/>*/}
-                {/*</Route>*/}
-                {/*<Route exact path='/chats' element={<PrivateRoute/>}>*/}
-                {/*    <Route exact path='/chats' element={<Chats/>}/>*/}
-                {/*</Route>*/}
-                {/*<Route exact path='/chats/:id' element={<PrivateRoute/>}>*/}
-                {/*    <Route exact path='/chats/:id' element={<Chat/>}/>*/}
-                {/*</Route>*/}
-                {/*<Route exact path='/signup' element={<SignUp/>}/>*/}
-                {/*<Route exact path='/login' element={<Login/>}/>*/}
             </Routes>
         </Fragment>
     </Router>
-
-    // <Router>
-    //     <Routes>
-    //         <Fragment>
-    //             <Route path="/login" element={<Login />} />
-    //
-    //             <ProtectedRoute path="/" component={<Home />} />
-    //             <ProtectedRoute path="/chats" component={<Chats />} />
-    //         </Fragment>
-    //     </Routes>
-    // </Router>
 );
