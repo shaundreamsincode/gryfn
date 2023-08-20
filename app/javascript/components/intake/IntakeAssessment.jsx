9908e9df223e653f8257a75db5508ecd50741dbc
Shaun Carland <shaundreamsincode@gmail.com>
2023-08-19 21:37:40 -0600

    add FE pagination

--- app/javascript/components/intake/IntakeAssessment.jsx
+++ app/javascript/components/intake/IntakeAssessment.jsx
@@ -1,51 +1,87 @@
-import React, { useState, useEffect }  from "react";
-import {CardContent, Button} from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { CardContent, Button } from "@material-ui/core";
 import ApiService from "../../services/ApiService";
 import IntakeQuestion from "./IntakeQuestion";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const IntakeAssessment = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
-    const currentUrl = window.location.href
-    const assessmentToken = currentUrl.split("/")[4]
-    const [assessment, setAssessment] = useState(null)
+    const currentUrl = window.location.href;
+    const assessmentToken = currentUrl.split("/")[4];
+
+    const questionsPerPage = 5; // Number of questions per page
+    const [currentPage, setCurrentPage] = useState(0);
+    const [assessment, setAssessment] = useState(null);
 
     const onQuestionSave = () => {
         ApiService.getIntakeAssessment(assessmentToken).then((response) => {
-            setAssessment(response.data)
-        })
-    }
+            setAssessment(response.data);
+        });
+    };
 
     const handleFinishButtonClick = () => {
-        navigate(`/intake_assessments/${assessmentToken}/summary`)
-    }
+        navigate(`/intake_assessments/${assessmentToken}/summary`);
+    };
+
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
+    };
 
     useEffect(() => {
         ApiService.getIntakeAssessment(assessmentToken).then((response) => {
-            setAssessment(response.data)
-        })
-    }, [assessmentToken])
+            setAssessment(response.data);
+        });
+    }, [assessmentToken]);
 
     if (!assessment) {
-        return(<CardContent>
-            Loading...
-        </CardContent>)
+        return (
+            <CardContent>
+                Loading...
+            </CardContent>
+        );
     }
 
-    const hasUnansweredQuestion = assessment.questions.some((question) => !question.answer);
+    const startIndex = currentPage * questionsPerPage;
+    const endIndex = startIndex + questionsPerPage;
+    const questionsToDisplay = assessment.questions.slice(startIndex, endIndex);
 
-    return(<CardContent>
-        <div>
-            {
-                assessment.questions.map((question) => {
-                    return (<IntakeQuestion question={question} onSave={onQuestionSave}/>)
-                })
-            }
-        </div>
+    const hasUnansweredQuestion = questionsToDisplay.some((question) => !question.answer);
 
-        <Button onClick={handleFinishButtonClick} disabled={hasUnansweredQuestion}>Finish</Button>
-    </CardContent>)
-}
+    return (
+        <CardContent>
+            <div>
+                {
+                    questionsToDisplay.map((question) => (
+                        <IntakeQuestion key={question.token} question={question} onSave={onQuestionSave} />
+                    ))
+                }
+            </div>
 
-export default IntakeAssessment
+            <div>
+                <Button
+                    onClick={() => handlePageChange(currentPage - 1)}
+                    disabled={currentPage === 0}
+                >
+                    Previous Page
+                </Button>
+                <Button
+                    onClick={() => handlePageChange(currentPage + 1)}
+                    disabled={endIndex >= assessment.questions.length}
+                >
+                    Next Page
+                </Button>
+            </div>
+
+            <Button onClick={handleFinishButtonClick} disabled={hasUnansweredQuestion}>Finish</Button>
+        </CardContent>
+    );
+};
+
+export default IntakeAssessment;
+
+
+
+
+
+
--- app/models/intake_question.rb
+++ app/models/intake_question.rb
@@ -3,9 +3,12 @@ class IntakeQuestion < ApplicationRecord
 
     FILE_NAMES =
       %w(
-    parliament.mp3
+    what.mp3
     knapsack.mp3
     foreign.mp3
     litigious.mp3
-    achieve.mp3)
+    triptych.mp3
+    execrable.mp3
+    islet.mp3
+    ).freeze
 end
