536bb6de570e6db9987211a4f7e05166851e4bea
Shaun Carland <shaundreamsincode@gmail.com>
2023-08-20 01:59:07 -0600

    add tweaks and validations to register form

--- app/javascript/components/onboarding/Register.jsx
+++ app/javascript/components/onboarding/Register.jsx
@@ -8,21 +8,29 @@ import {
     FormControl,
     FormLabel,
     RadioGroup,
-    FormControlLabel, Radio, InputLabel, Select, MenuItem
+    FormControlLabel,
+    Radio,
+    InputLabel,
+    Select,
+    MenuItem,
 } from "@material-ui/core";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import ApiService from "../../services/ApiService";
 
 const Register = () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [birthYear, setBirthYear] = useState('')
-    const [country, setCountry] = useState('')
-    const [zipCode, setZipCode] = useState('')
-    const [previouslyDiagnosed, setPreviouslyDiagnosed] = useState(null)
-    const [levelOfEducation, setLevelOfEducation] = useState(null)
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [birthYear, setBirthYear] = useState("");
+    const [country, setCountry] = useState("");
+    const [zipCode, setZipCode] = useState("");
+    const [previouslyDiagnosed, setPreviouslyDiagnosed] = useState(null);
+    const [levelOfEducation, setLevelOfEducation] = useState(null);
 
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const navigate = useNavigate();
+
+    const [isEmailValid, setIsEmailValid] = useState(true);
+    const [emailErrorMessage, setEmailErrorMessage] = useState("");
 
     const handleSubmit = () => {
         const params = {
@@ -32,21 +40,48 @@ const Register = () => {
             country: country,
             zip_code: zipCode,
             previously_diagnosed: previouslyDiagnosed,
-            level_of_education: levelOfEducation
-        }
+            level_of_education: levelOfEducation,
+        };
 
         ApiService.createIntakeAssessment(params).then((response) => {
-            navigate(`/intake_assessments/${response.data.intakeAssessmentToken}`)
-        })
-    }
+            navigate(`/intake_assessments/${response.data.intakeAssessmentToken}`);
+        });
+    };
 
-    return(
+    const nextButtonDisabled = () => {
+        return !(
+            name &&
+            isEmailValid &&
+            birthYear &&
+            country &&
+            zipCode &&
+            previouslyDiagnosed &&
+            levelOfEducation
+        );
+    };
+
+    return (
         <CardContent>
             <div>
                 <TextField value={name} label="First Name" onChange={(e) => setName(e.target.value)} />
             </div>
             <div>
-                <TextField value={email} label="Email" onChange={(e) => setEmail(e.target.value)} />
+                <TextField
+                    value={email}
+                    label="Email"
+                    onChange={(e) => {
+                        const input = e.target.value;
+                        setEmail(input);
+                        if (emailRegex.test(input)) {
+                            setIsEmailValid(true);
+                            setEmailErrorMessage("");
+                        } else {
+                            setIsEmailValid(false);
+                            setEmailErrorMessage("Invalid email format");
+                        }
+                    }}
+                />
+                {!isEmailValid && <Typography color="error">{emailErrorMessage}</Typography>}
             </div>
             <div>
                 <TextField
@@ -67,14 +102,16 @@ const Register = () => {
                 <TextField value={zipCode} label="Zip Code" onChange={(e) => setZipCode(e.target.value)} />
             </div>
 
-            <div style={{ 'marginTop': '40px' }}>
+            <div style={{ marginTop: "40px" }}>
                 <FormControl component="fieldset">
                     <FormLabel component="legend">Have you been diagnosed with a learning disorder?</FormLabel>
                     <RadioGroup
                         aria-label="options"
                         name="options"
                         value={previouslyDiagnosed}
-                        onChange={(e) => { setPreviouslyDiagnosed(e.target.value) }}
+                        onChange={(e) => {
+                            setPreviouslyDiagnosed(e.target.value);
+                        }}
                     >
                         <FormControlLabel value="option1" control={<Radio />} label="Yes" />
                         <FormControlLabel value="option2" control={<Radio />} label="No" />
@@ -82,33 +119,38 @@ const Register = () => {
                 </FormControl>
             </div>
 
-            <div style={{'marginTop' : '20px' }}>
+            <div style={{ marginTop: "20px" }}>
                 <FormControl>
                     <FormLabel component="legend">What is your highest level of education?</FormLabel>
-                    <InputLabel style={{'marginTop' : '20px' }}>Select an option</InputLabel>
+                    <InputLabel style={{ marginTop: "20px" }}>Select an option</InputLabel>
                     <Select
                         value={levelOfEducation}
-                        onChange={(e) => { setLevelOfEducation(e.target.value) }}
+                        onChange={(e) => {
+                            setLevelOfEducation(e.target.value);
+                        }}
                         label="Select an option"
                     >
                         <MenuItem value="">
                             <em>None</em>
                         </MenuItem>
-                        <MenuItem value="option1">Option 1</MenuItem>
-                        <MenuItem value="option2">Option 2</MenuItem>
-                        <MenuItem value="option3">Option 3</MenuItem>
+                        <MenuItem value="elementary_school">Elementary School</MenuItem>
+                        <MenuItem value="middle_school">Middle School</MenuItem>
+                        <MenuItem value="high_school">High School</MenuItem>
+                        <MenuItem value="associates">Associates</MenuItem>
+                        <MenuItem value="bachelors">Bachelors</MenuItem>
+                        <MenuItem value="masters">Masters</MenuItem>
+                        <MenuItem value="phd">PhD</MenuItem>
                     </Select>
                 </FormControl>
             </div>
 
             <div style={{ display: "flex", justifyContent: "flex-end", marginTop: "20px" }}>
-                <Button variant="contained" color="primary" onClick={handleSubmit}>
-                {/*<Button variant="contained" color="primary" onClick={handleSubmit} disabled={!(name && email && birthYear && country && zipCode && password && passwordConfirmation)}>*/}
+                <Button variant="contained" color="primary" onClick={handleSubmit} disabled={nextButtonDisabled()}>
                     Next
                 </Button>
             </div>
         </CardContent>
-    )
-}
+    );
+};
 
-export default Register
+export default Register;
