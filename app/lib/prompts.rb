2e0ec7d863f555009a3e2dc32ea4cdf55640ac7e
Shaun Carland <shaundreamsincode@gmail.com>
2023-08-20 01:24:19 -0600

    remove old references to docbot

--- app/lib/prompts.rb
+++
@@ -1,7 +0,0 @@
-module Prompts
-  INITIAL_SYSTEM_PROMPT = "I want you to act as an AI assisting a doctor.  You will gather the history of present illness from the patient. This includes asking the patient more about their symptoms and medical conditions to better understand their problems. Only ask one question at a time and let the patient answer before asking another question. Ask as many clarifying questions as you can. DO NOT OFFER MEDICAL ADVICE. If the patient is in need of emergency, tell them to contact 911."
-  SUMMARY_PROMPT = "Please summarize the chat in the format of a history of present illness with bullet points."
-
-  INITIAL_ASSISTANT_PROMPT_EN = "Welcome to DocBot! I'm here to learn about your symptoms. Please feel free to share any discomfort or concerns you're experiencing, and we'll work together to provide a helpful summary of your symptoms for your doctor."
-  INITIAL_ASSISTANT_PROMPT_ES = "¡Bienvenido a DocBot! Estoy aquí para conocer sus síntomas. No dude en compartir cualquier molestia o inquietud que tenga, y trabajaremos juntos para brindarle a su médico un resumen útil de sus síntomas."
-end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_docbot_app_session'
+Rails.application.config.session_store :cookie_store, key: '_gryfn_app_session'
