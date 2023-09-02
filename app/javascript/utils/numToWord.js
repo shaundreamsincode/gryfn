abd60f8798fa4fe59cee53ed48c47963dfe65a24
Shaun Carland <shaundreamsincode@gmail.com>
2023-09-01 19:24:14 -0600

    remove unused javascript utils

--- app/javascript/utils/numToWord.js
+++
@@ -1,7 +0,0 @@
-import numWords from 'num-words';
-
-const convertNumberToWords = (input) => {
-  return input.replace(/\d+/g, (number) => numWords(number));
-};
-
-export default convertNumberToWords;
