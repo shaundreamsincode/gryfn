0729d34caa09d00335f7e30b6683b45f9e6b7b84
Shaun Carland <shaundreamsincode@gmail.com>
2023-09-05 18:59:34 -0600

    remove old migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_230939) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_045935) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,16 +73,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_230939) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["intake_assessment_id"], name: "index_phonetic_quest_on_assessment_idx"
-  end
-
-  create_table "intake_speech_questionnaires", force: :cascade do |t|
-    t.integer "grade"
-    t.integer "current_level"
-    t.integer "current_question_id"
-    t.string "correct_words", default: [], null: false, array: true
-    t.string "incorrect_words", default: [], null: false, array: true
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "intake_speech_questions", force: :cascade do |t|
