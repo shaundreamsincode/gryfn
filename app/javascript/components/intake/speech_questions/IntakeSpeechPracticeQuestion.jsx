c3d5ac1372499c17f744c43713b0ee27dc3251ca
Shaun Carland <shaundreamsincode@gmail.com>
2023-09-07 14:36:24 -0600

    cleanup with chatgpt

--- app/javascript/components/intake/speech_questions/IntakeSpeechPracticeQuestion.jsx
+++ app/javascript/components/intake/speech_questions/IntakeSpeechPracticeQuestion.jsx
@@ -1,85 +1,80 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ApiService from "../../../services/ApiService";
-import {AudioRecorder} from "react-audio-voice-recorder";
-import {Button, Card, CardContent, Typography, Snackbar} from "@material-ui/core";
-import {Alert, Container} from "@mui/material";
+import { AudioRecorder } from "react-audio-voice-recorder";
+import {
+    Button,
+    Card,
+    CardContent,
+    Typography,
+    Snackbar,
+} from "@material-ui/core";
+import { Alert } from "@mui/material";
 
 const IntakeSpeechPracticeQuestion = (props) => {
-    const { onSolveProp, assessmentToken } = props
-    // const [practiceQuestionSolved, setPracticeQuestionSolved] = useState(false)
+    const { onSolveProp, assessmentToken } = props;
 
-    const [isSaving, setIsSaving] = useState(false)
-    const [blob, setBlob] = useState(null)
-    const [questionAnswered, setQuestionAnswered] = useState(false)
-
-    const [recordingFailed, setRecordingFailed] = useState(false)
-    const [recordingFailedCount, setRecordingFailedCount] = useState(0)
-    const [incorrectAnswer, setIncorrectAnswer] = useState(null)
-
-    const [showRecordingSuccessMessage, setShowRecordingSuccessMessage] = useState(false)
-
-    const microphoneStyle = () => {
-        const disabledMicrophoneStyle = {
-            "pointer-events": "none",
-            "opacity": "0.5"
-        }
-
-        if (questionAnswered) {
-            return disabledMicrophoneStyle
-        } else {
-            return {}
-        }
-    }
+    const [isSaving, setIsSaving] = useState(false);
+    const [questionAnswered, setQuestionAnswered] = useState(false);
+    const [recordingFailed, setRecordingFailed] = useState(false);
+    const [recordingFailedCount, setRecordingFailedCount] = useState(0);
+    const [incorrectAnswer, setIncorrectAnswer] = useState(null);
+    const [showRecordingSuccessMessage, setShowRecordingSuccessMessage] = useState(
+        false
+    );
 
     const onRecordingComplete = (blob) => {
-        setIsSaving(true)
-        setBlob(blob)
-        const wavFromBlob = new File([blob], "test.wav")
-        ApiService.practiceSpeechQuestions(assessmentToken, wavFromBlob).then((response) => {
-            setIsSaving(false)
+        setIsSaving(true);
+        const wavFromBlob = new File([blob], "test.wav");
 
-            setQuestionAnswered(true)
-            setShowRecordingSuccessMessage(true)
-        }).catch((error) => {
-            setIsSaving(false)
+        ApiService.practiceSpeechQuestions(assessmentToken, wavFromBlob)
+            .then((response) => {
+                setIsSaving(false);
+                setQuestionAnswered(true);
+                setShowRecordingSuccessMessage(true);
+            })
+            .catch((error) => {
+                setIsSaving(false);
 
-            const errorData = error.response.data
-            const errorCode = errorData.error
+                const errorData = error.response.data;
+                const errorCode = errorData.error;
 
-            if (errorCode === 'incorrect_answer') {
-                setIncorrectAnswer(errorData.decoded_answer)
-            }
-
-            if (errorCode === 'decode_failed') {
-                setRecordingFailed(true)
-                setRecordingFailedCount(recordingFailedCount + 1)
-            }
-        })
-    }
-
-    return(<Card>
-            <CardContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
-                <Typography variant="h4">
-                    Practice Question
-                </Typography>
-
-                <Typography>
-                    Say the word "map".
-                </Typography>
-
-                {
-                    recordingFailedCount > 4 && <Typography style={{ color: 'red' }}>Your microphone has failed quite a few times in a row. Perhaps you should check it?</Typography>
-
-                }
-                {
-                    questionAnswered && <Typography style={{ color: 'g' }}>Question Answered!</Typography>
-                }
-                {
-                    isSaving && <Typography>Saving...</Typography>
+                if (errorCode === "incorrect_answer") {
+                    setIncorrectAnswer(errorData.decoded_answer);
                 }
 
-                {
-                    !isSaving && !questionAnswered && <div onClick={() => { setIsRecording(true) }} style={microphoneStyle()}>
+                if (errorCode === "decode_failed") {
+                    setRecordingFailed(true);
+                    setRecordingFailedCount(recordingFailedCount + 1);
+                }
+            });
+    };
+
+    const microphoneStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center',
+    };
+
+    return (
+        <Card>
+            <CardContent style={microphoneStyle}>
+                <Typography variant="h4">Practice Question</Typography>
+                <Typography>Say the word "map".</Typography>
+
+                {recordingFailedCount > 4 && (
+                    <Typography style={{ color: "red" }}>
+                        Your microphone has failed quite a few times in a row. Perhaps you
+                        should check it?
+                    </Typography>
+                )}
+                {questionAnswered && (
+                    <Typography style={{ color: "green" }}>Question Answered!</Typography>
+                )}
+                {isSaving && <Typography>Saving...</Typography>}
+
+                {!isSaving && !questionAnswered && (
+                    <div onClick={() => setIsRecording(true)}>
                         <AudioRecorder
                             onRecordingComplete={onRecordingComplete}
                             audioTrackConstraints={{
@@ -89,20 +84,26 @@ const IntakeSpeechPracticeQuestion = (props) => {
                             downloadFileExtension="webm"
                         />
                     </div>
-                }
+                )}
 
                 <div style={{ marginTop: '40px' }}>
-                    <Button onClick={() => { onSolveProp() }} disabled={!questionAnswered} color="primary" variant="contained">Start Assessment</Button>
+                    <Button
+                        onClick={() => onSolveProp()}
+                        disabled={!questionAnswered}
+                        color="primary"
+                        variant="contained"
+                    >
+                        Start Assessment
+                    </Button>
                 </div>
             </CardContent>
+
             <Snackbar
                 open={showRecordingSuccessMessage}
                 autoHideDuration={6000}
                 onClose={() => setShowRecordingSuccessMessage(false)}
             >
-                <Alert severity="success">
-                    Recording was successful!
-                </Alert>
+                <Alert severity="success">Recording was successful!</Alert>
             </Snackbar>
 
             <Snackbar
@@ -110,10 +111,9 @@ const IntakeSpeechPracticeQuestion = (props) => {
                 autoHideDuration={6000}
                 onClose={() => setRecordingFailed(false)}
             >
-                <Alert severity="error">
-                    Recording failed! Please try again!
-                </Alert>
+                <Alert severity="error">Recording failed! Please try again!</Alert>
             </Snackbar>
+
             <Snackbar
                 open={!!incorrectAnswer}
                 autoHideDuration={6000}
@@ -125,8 +125,7 @@ const IntakeSpeechPracticeQuestion = (props) => {
                 </Alert>
             </Snackbar>
         </Card>
-    )
+    );
+};
 
-}
-
-export default IntakeSpeechPracticeQuestion
+export default IntakeSpeechPracticeQuestion;
