997ba85aa0ddedd3442a0b60739de17adeba144c
Shaun Carland <shaundreamsincode@gmail.com>
2023-09-09 21:16:28 -0600

    remove comments

--- app/interactors/intake_assessments/perform_registration.rb
+++ app/interactors/intake_assessments/perform_registration.rb
@@ -6,7 +6,6 @@ module IntakeAssessments
       intake_assessment = context.intake_assessment
 
       intake_assessment.update!(
-        # speech_assessment_current_level: 0,
         assessment_type: calculate_assessment_type,
         email: context.email,
         birth_year: context.birth_year,
@@ -14,7 +13,6 @@ module IntakeAssessments
         zip_code: context.zip_code,
         previously_diagnosed: context.previously_diagnosed,
         level_of_education: context.level_of_education,
-        # current_speech_question_index: 0
       )
 
       IntakeAssessments::CreateSpeechQuestions.call(assessment: intake_assessment)
