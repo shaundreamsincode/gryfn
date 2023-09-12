408eaa1aaa0dae3abad518734e6bd5532ca8e311
Shaun Carland <shaundreamsincode@gmail.com>
2023-09-11 23:58:21 -0600

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,38 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 OnboardOrganization.call(
-  organization_name: 'ACME corp',
+  organization_name: "Corinne's Org",
   account_first_name: 'Corinne',
   account_last_name: 'Carland',
   account_email: 'corinne@gmail.com',
-  account_password: 'Password123!',
-  account_password_confirmation: 'Password123!'
+  account_password: 'passworded',
+  account_password_confirmation: 'passworded'
 )
 
+OnboardOrganization.call(
+  organization_name: "Edo's Org",
+  account_first_name: 'Edo',
+  account_last_name: 'Idk',
+  account_email: 'edo@gmail.com',
+  account_password: 'passworded',
+  account_password_confirmation: 'passworded'
+)
+
+
+OnboardOrganization.call(
+  organization_name: "Gryff's Org",
+  account_first_name: 'Gryff',
+  account_last_name: 'Idk',
+  account_email: 'gryff@gmail.com',
+  account_password: 'passworded',
+  account_password_confirmation: 'passworded'
+)
+
+OnboardOrganization.call(
+  organization_name: "Shaun's Org",
+  account_first_name: 'Shaun',
+  account_last_name: 'Carland',
+  account_email: 'shauncarland@gmail.com',
+  account_password: 'passworded',
+  account_password_confirmation: 'passworded'
+)
